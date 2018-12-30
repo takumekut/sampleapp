4f85f813f7e9947b0a9708049ed3a4ac4802321c
takumekut <rokudenasi8452@gmail.com>
2018-12-30 10:12:50 +0000

    contact add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,7 @@ class ApplicationController < ActionController::Base
   def hello
     render html:"hello, world!"
   end
+  
+  def contact
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -32,5 +32,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 end
+
+  
